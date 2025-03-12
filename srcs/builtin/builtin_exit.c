f2f9aeaa6f50970a32f1d731952e27de1d2db953
kate saegusa <98626554+3edq@users.noreply.github.com>
2025-03-12 18:17:57 +0900

    Update builtin_exit.c

--- srcs/builtin/builtin_exit.c
+++ srcs/builtin/builtin_exit.c
@@ -1,23 +1,5 @@
 #include "../include/minishell.h"
 
-// int	builtin_exit(char **args)
-// {
-// 	int	exit_status;
-
-// 	if (args[1])
-// 	{
-// 		exit_status = ft_atoi(args[1]);
-// 		if (exit_status == 0 && args[1][0] != '0')
-// 		{
-// 			ft_putstr_fd("minishell: exit: numeric argument required\n",
-// 				STDERR_FILENO);
-// 			exit(255);
-// 		}
-// 		exit(exit_status);
-// 	}
-// 	exit(0);
-// }
-
 static int	is_numeric(const char *str)
 {
 	int	i;
