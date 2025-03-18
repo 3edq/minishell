c03eff9e4c722560b2ea79c8a73f27febef43b5f
kate saegusa <98626554+3edq@users.noreply.github.com>
2025-03-18 15:43:32 +0900

    Update path.c

--- srcs/executer/path.c
+++ srcs/executer/path.c
@@ -46,44 +46,54 @@ static char	*build_path(char *dir, char *cmd)
 	return (NULL);
 }
 
-static char	*try_path(char **paths, char *cmd)
+static char *try_path(char **paths, char *cmd)
 {
-	char	*path;
-	int		i;
+   char    *path;
+   int     i;
 
-	i = 0;
-	while (paths[i])
-	{
-		path = build_path(paths[i], cmd);
-		if (path)
-		{
-			free_tab(paths);
-			return (path);
-		}
-		i++;
-	}
-	free_tab(paths);
-	return (NULL);
+
+   i = 0;
+   while (paths[i])
+   {
+       path = build_path(paths[i], cmd);
+       if (path && access(path, X_OK) == 0)
+       {
+           free_tab(paths);
+           return (path);
+       }
+       free(path);
+       i++;
+   }
+   free_tab(paths);
+   return (NULL);
 }
 
-char	*find_path(char *cmd, char **envp)
-{
-	char	**paths;
-	char	*path_env;
 
-	if (!cmd || cmd[0] == '\0')
-		return (NULL);
-	if (cmd[0] == '/' || (cmd[0] == '.' && (cmd[1] == '/' || cmd[1] == '.')))
-		return (build_path(NULL, cmd));
-	path_env = getenv("PATH");
-	while (*envp && ft_strncmp(*envp, "PATH=", 5) != 0)
-		envp++;
-	if (*envp)
-		path_env = *envp + 5;
-	if (!path_env)
-		return (NULL);
-	paths = ft_split(path_env, ':');
-	if (!paths)
-		return (NULL);
-	return (try_path(paths, cmd));
+char    *find_path(char *cmd, char **envp)
+{
+   char    **paths;
+   char    *path_env;
+
+
+   if (!cmd || cmd[0] == '\0')
+       return (NULL);
+   if (cmd[0] == '/' || (cmd[0] == '.' && (cmd[1] == '/' || cmd[1] == '.')))
+       return (build_path(NULL, cmd));
+   path_env = NULL;
+   while (*envp)
+   {
+       if (ft_strncmp(*envp, "PATH=", 5) == 0)
+       {
+           path_env = *envp + 5;
+           break ;
+       }
+       envp++;
+   }
+   if (!path_env || path_env[0] == '\0')
+       return (NULL);
+   paths = ft_split(path_env, ':');
+   if (!paths)
+       return (NULL);
+   return (try_path(paths, cmd));
 }
+
