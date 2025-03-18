1372d4a605dd6697193a2c29a5dec4b017c22ee9
KWEKE633 <iloveigo148mensa@gmail.com>
2025-03-18 15:47:56 +0900

    Update builtin_echo.c

--- srcs/builtin/builtin_echo.c
+++ srcs/builtin/builtin_echo.c
@@ -19,8 +19,8 @@ int	builtin_echo(char **args)
 	int	j;
 
 	newline = 1;
-	i = 1;
-	while (args[i] && args[i][0] == '-' && args[i][1] == 'n')
+	i = 0;
+	while (args[++i] && args[i][0] == '-' && args[i][1] == 'n')
 	{
 		j = 1;
 		while (args[i][j] == 'n')
@@ -28,13 +28,13 @@ int	builtin_echo(char **args)
 		if (args[i][j] != '\0')
 			break ;
 		newline = 0;
-		i++;
 	}
 	while (args[i])
 	{
 		ft_putstr_fd(args[i], STDOUT_FILENO);
-		if (args[i++ + 1])
+		if (args[i + 1])
 			ft_putchar_fd(' ', STDOUT_FILENO);
+		i++;
 	}
 	if (newline)
 		ft_putchar_fd('\n', STDOUT_FILENO);
